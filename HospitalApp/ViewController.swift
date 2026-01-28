5acee0ca4427edd4297c6a41e8a7d261f659d654
duanehomick <duane@homick.com>
2026-01-28 13:06:41 -0500

    Initial Commit

---
+++ HospitalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HospitalApp
+//
+//  Created by Duane Homick on 2026-01-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
